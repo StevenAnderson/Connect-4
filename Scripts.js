9a048509577fe17808953e5f624c59afa3a31317
Steven Anderson <sanderson17@my.whitworth.edu>
2017-11-16 23:13:02 -0700

    Pre-Testing submit
    locked board, username submitting

--- Scripts.js
+++ Scripts.js
@@ -7,7 +7,7 @@ var win=0;
 var winningPlayer=0;
 var pieceLocation=-1;
 var winners=[];
-
+var gameOver=false;
 
 var board = [[0,0,0,0,0,0],
              [0,0,0,0,0,0],
@@ -24,13 +24,14 @@ function placePiece(column){
   var draw=false;
   var i=6
   var placement=6;
+  if(gameOver==false){
   for(i;i>=0;i--){
-    if(board[i][column-1]==0){
+    if(board[i][column-1]===0){
 
         board[i][column-1]=currentPlayer;
         var id= i +" "+ column; /*sets up the variable as a string*/
 
-        if(currentPlayer==1){
+        if(currentPlayer===1){
           document.getElementById(id).src='red1.png';
           currentPlayer=2;
           document.getElementById('p1').src='redplayer2.png';
@@ -46,7 +47,7 @@ function placePiece(column){
         placement=i;
         i=0;
       }
-    else if(i==0){
+    else if(i===0){
       /*no placement allowed*/
       alert("Please click on a valid placement.");
     }
@@ -66,26 +67,45 @@ function placePiece(column){
   if(checkDia2(board,column, placement)){
     winningMove=true;
   }
-  if(winningMove==true){
+  if(winningMove===true){
     var winPlayer;
+    gameOver=true;
     document.getElementById('winDraw').style.visibility='visible';
-    if(currentPlayer==1){
-      winPlayer='Yellow';
+    if(currentPlayer===1){
+        var namep2=document.getElementById('input2').value ;
+        if (namep2.length<25){
+          winPlayer=namep2;
+        }
+        else {
+          winPlayer='Yellow Player';
+          document.getElementById('input2').value='Yellow Player';
+        }
       winners.push(2);
     }
     else {
-      winPlayer='Red';
-      winners.push(1);
+      var namep1=document.getElementById('input1').value ;
+      if (namep1.length<25){
+        winPlayer=namep1;
+      }
+      else {
+        winPlayer='Red Player';
+        document.getElementById('input1').value='Red Player';
+      }
+         winners.push(1);
     }
-    document.getElementById('winDraw').innerHTML= winPlayer + " Player Wins!";
+
+    document.getElementById('winDraw').innerHTML= winPlayer + " Wins!";
     playAgain();
   }
 
-  if(draw==true && winningMove==false){
+  if(draw===true && winningMove===false){
     document.getElementById('winDraw').style.visibility='visible';
     document.getElementById('winDraw').innerHTML='A Draw!' ;
     playAgain();
+    gameOver=true;
   }
+}
+
 }
 
 function playAgain(){
@@ -96,7 +116,7 @@ function playAgain(){
   var p1Wins=0;
   var p2Wins=0;
   for(var i=0;i<winners.length;i++){
-    if(winners[i]==1){
+    if(winners[i]===1){
       p1Wins++;
     }
     else {
@@ -121,8 +141,8 @@ function checkHor(board,column,placement){
     var piece=board[placement][column-1];
     for (var j=0;j<=2;j++){
       var val=board[placement][j];
-      if (val!==0 && board[placement][j+1]==val && board[placement][j+2]==val
-        && board[placement][j+3]==val){
+      if (val!==0 && board[placement][j+1]===val && board[placement][j+2]===val
+        && board[placement][j+3]===val){
           return true;
       }
     }
@@ -133,8 +153,8 @@ function checkVer(board,column,placement){
     var piece=board[placement][column-1];
     for (var j=0;j<=3;j++){
       var val=board[j][column-1];
-      if (val!==0 && board[j+1][column-1]==val && board[j+2][column-1]==val
-        && board[j+3][column-1]==val){
+      if (val!==0 && board[j+1][column-1]===val && board[j+2][column-1]===val
+        && board[j+3][column-1]===val){
           return true;
       }
     }
@@ -148,8 +168,8 @@ function checkDia1(board,column,placement){
       for (var j=0;j<=2;j++){
         for (var y=0;y<=3;y++){
         var val=board[y][j];
-        if (val!==0 && board[y+1][j+1]==val && board[y+2][j+2]==val
-          && board[y+3][j+3]==val){
+        if (val!==0 && board[y+1][j+1]===val && board[y+2][j+2]===val
+          && board[y+3][j+3]===val){
             return true;
         }
       }
@@ -162,8 +182,8 @@ function checkDia2(board,column,placement){
   for (var j=0;j<=2;j++){
     for (var y=3;y<=6;y++){
     var val=board[y][j];
-    if (val!==0 && board[y-1][j+1]==val && board[y-2][j+2]==val
-      && board[y-3][j+3]==val){
+    if (val!==0 && board[y-1][j+1]===val && board[y-2][j+2]===val
+      && board[y-3][j+3]===val){
         return true;
     }
   }
@@ -175,11 +195,11 @@ function checkDraw(board){
   var check=0;
   for (var i=0;i<=5;i++){
     for (var j=0; j<=6;j++){
-      if (board[j][i]==0)
+      if (board[j][i]===0)
         check=1;
     }
   }
-  if(check==0)
+  if(check===0)
     return true;
   return false;
 }
@@ -191,7 +211,8 @@ function removeHover(column){
 
 function theRules() {
     alert("Object:    To win Connect Four you must be the first player to get four of your colored checkers in a row either horizontally, vertically or diagonally. \n"+
-  "\nHover above the board and click to drop your piece into place. Good Luck!");
+  "\nHover above the board and click to drop your piece into place. Good Luck!" +
+  "\n \nReplace 'Red/Yellow player' with whichever name you desire. Please keep it under 25 characters though.");
   }
 
 function clearBoard(){
@@ -206,6 +227,7 @@ function clearBoard(){
                [0,0,0,0,0,0],
                [0,0,0,0,0,0],
                [0,0,0,0,0,0]];
+  gameOver=false;
     for(var i=0;i<=6;i++){
       for(var j=1;j<=6;j++){
         var id= i +" "+ j; /*sets up the variable as a string*/
@@ -216,7 +238,7 @@ function clearBoard(){
 
 function hoverPiece(column){
   var id='ic'+column;
-  if(currentPlayer==1){
+  if(currentPlayer===1){
     var check= document.getElementById(id).src='redhover.png';
   }
   else {
